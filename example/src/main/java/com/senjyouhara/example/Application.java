98a0c5f412dce0bd1e0e6e71892c61210553c795
liuzijun <lzj724@qq.com>
2020-08-06 10:01:53 +0800

    添加cookise请求传递

--- example/src/main/java/com/senjyouhara/example/Application.java
+++ example/src/main/java/com/senjyouhara/example/Application.java
@@ -3,7 +3,6 @@ package com.senjyouhara.example;
 import com.senjyouhara.crawler.annotation.EnabledCrawler;
 import com.senjyouhara.crawler.confg.CrawlerContext;
 import com.senjyouhara.example.reptile.Bilibili;
-import com.senjyouhara.example.reptile.Cordcloud;
 import org.jasypt.encryption.StringEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -17,6 +16,5 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class,args);
-		CrawlerContext.run(Cordcloud.class);
 	}
 }
